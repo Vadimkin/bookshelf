bfe1b07f45781580e528c3f223a4c69e39775e7e
Vadim <mail@vadimklimenko.com>
2014-07-20 15:00:55 +0300

    Write url pattern for books

--- bookshelf/urls.py
+++ bookshelf/urls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import patterns, include, url
-
 from django.conf import settings
 from django.conf.urls.static import static
-
 from django.contrib import admin
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -12,5 +11,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('books.urls')),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT})
 )
