1d52fa625bcf60e85cec12d4184b1f83d9168129
Vadim <mail@vadimklimenko.com>
2014-07-20 12:55:41 +0300

    Update Book model && adding admin page

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    fields = (('author', 'title'), 'book_pic', ('date_reading', 'favorite'), 'action', 'review')
+
+admin.site.register(Book, BookAdmin)
--- books/models.py
+++ books/models.py
@@ -1,3 +1,22 @@
-from django.db import models
+# -*- coding: utf-8 -*- 
 
-# Create your models here.
+from django.db import models
+from datetime import date
+
+class Book(models.Model):
+	BUTTON_TYPES = [(1, u'Прочитанное'), (2, u'Читаю'), (3, u'Хочу прочитать')]
+
+	author = models.CharField(max_length=200,  verbose_name=u'Автор')
+	title = models.CharField(max_length=200, verbose_name=u'Название книжки')
+	book_pic = models.ImageField(upload_to="books/", default='books/nopic.jpg', verbose_name=u'Изображение')
+	favorite = models.BooleanField(default='False', verbose_name=u'В избранном')
+	action = models.IntegerField(choices=BUTTON_TYPES, default=1, verbose_name=u'Действие')
+	date_reading = models.DateField(verbose_name=u'Дата прочтения', default=date.today)
+	review = models.TextField(verbose_name=u'Отзыв', default='')
+
+	def __unicode__(self):
+		return u"{0} ({1})".format(self.author, self.title)
+
+	class Meta:
+		verbose_name = "книжку"
+		verbose_name_plural = "книжка"
